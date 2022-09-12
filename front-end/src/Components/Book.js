a80e1b54c52a1d176aa3b1f478630a01cea7f8cb
T.K <nique@niques-air.myfiosgateway.com>
2022-09-12 19:43:31 -0400

    front-end deployment

--- front-end/src/Components/Book.js
+++ front-end/src/Components/Book.js
@@ -6,7 +6,7 @@ export default function Book({ book }) {
         <Link className="Book" to={`/books/${book.id}`}> <img src={book.image} alt="" className="resize" />
         </Link>
       <div className="card-body">
-        <Link to={`/books/${book.id}/edit`}><img src="https://img.icons8.com/arcade/64/000000/experimental-edit-arcade.png"/></Link>
+        <Link to={`/books/${book.id}/edit`}><img src="https://img.icons8.com/arcade/64/000000/experimental-edit-arcade.png" alt=''/></Link>
       </div>
     </div>
   )
